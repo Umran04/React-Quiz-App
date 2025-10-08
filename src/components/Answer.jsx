f680a123da8bcb307b71aae1e4dee574bf8e3db7
Umran <mohammedumran.0416@gmail.com>
2025-10-08 18:46:02 +0100

    correctly shows which answer has been selected for which question

---
+++ src/components/Answer.jsx
@@ -0,0 +1,16 @@
+
+import clsx from "clsx"
+
+
+export default function Answer(props){
+    return(
+        <>
+            <button className={clsx("answer--btn",{isHeld : props.isHeld})} onClick={props.onClick} >{props.answer}</button>
+        </>
+    )
+}
+
+//className={clsx("answer--btn",{isHeld : props.isHeld, isWrong : (props.isWrong), isCorrect : (props.isCorrect)})} 
+//the SELECTED answe still show the green, so if user selects an answe and it is correct then it stays green and is the only coloured button for that question
+//if the answer SELECTED is wrong, it still shows the same design as the correct answer but there  will be another button that will be in a red colour indicating
+//to the user that thier selected answer was wrong
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,42 +1,74 @@
 import React from "react"
+import Answer from "./Answer"
 import { useState,useEffect } from "react"
-import { nanoid } from "nanoid"
+
 
 export default function Quiz(){
 
     const [questions,setQuestions] = useState([])
-    const [answers,setAnswers] = useState([])
-    setAnswers
+    const [selectedAnswers, setSelectedAnswers] = useState({});
+    const [isCorrect, setIsCorrect] = useState()
 
-   
 
     useEffect(() => {
         async function fetchQuestions() {
             const res = await fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple');
             const data = await res.json();
-            setQuestions(data.results); 
+            setQuestions(data.results.map(q => {
+                return {
+                    ...q, 
+                    allAnswers: [...q.incorrect_answers, q.correct_answer].sort(() => Math.random() - 0.5)
+                };
+            }))
             
         }
     
         fetchQuestions(); 
     }, []);
+ 
 
+    
 
-    console.log(questions)
-
-    const questionEl = questions.map(q => {
-        const allAnswers = [...q.incorrect_answers, q.correct_answer].sort(()=> Math.random() - 0.5);
+    function holdAnswer(questionIndex, answerIndex) {
+        setSelectedAnswers(prev => {
+         
+          if (prev[questionIndex] === answerIndex) {
+            const newAnswers = { ...prev };
+            delete newAnswers[questionIndex]; 
+            return newAnswers;
+          } else {
+            
+            return {
+              ...prev,
+              [questionIndex]: answerIndex
+            };
+          }
+        });
+      }
+      
+    
+    const questionEl = questions.map((q, questionIndex) => {
         
-        return(
-        <React.Fragment key={nanoid()}>
-            <main className="question--answer--component">
+      return (
+            <div key={q.question} className="question--answer--component">
                 <h4>{q.question}</h4>
-                    <div className="btn-container">{allAnswers.map(answer => <button className="answer--btn" key={nanoid()}>{answer}</button>)}</div>
-                <hr></hr>
-            </main>
-        </React.Fragment>
-        )
-    })
+    
+                <div className="btn-container">
+                    {q.allAnswers.map((answer, answerIndex) => (
+                        <Answer
+                            key={answerIndex}
+                            id={answerIndex}
+                            onClick={() => holdAnswer(questionIndex, answerIndex)}
+                            isHeld={selectedAnswers[questionIndex] === answerIndex}
+                            answer={answer}
+                        />
+                    ))}
+                </div>
+                <hr />
+            </div>
+        );
+    });
+    
 
    
     
@@ -48,12 +80,13 @@ export default function Quiz(){
     return(
         <>
         <main className="question--component--main">
-            {/* ADD QUESTIONS */}
-            {/* ADD ANSWERS */}
-            {/* ADD LINE BREAK? */}
+            
 
             {questionEl}
-            {/*<button>Check Answers</button>*/}
+            {Object.keys(selectedAnswers).length === questions.length && (
+                <button>Check Answers</button>
+            )}
+
             
             
             
